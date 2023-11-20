6021947daea4538d2eb426cb1248ac194a7429f1
Dwaraka Nath <dtsdwarak@hotmail.com>
2023-11-20 15:52:21 +0530

    Add Query params

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Welcome to Amazonia - Amazon product exploration API",
+		firstname := c.DefaultQuery("firstname", "you")
+		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome " + firstname + " " + lastname + " to Amazonia - Amazon product exploration API",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
